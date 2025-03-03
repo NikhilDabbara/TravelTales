94b538bba7cc5da1ee0e55abd06c503d63ac363c
NikhilDabbara <sreenikhilchowdary@gmail.com>
2025-03-03 12:09:45 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,19 @@
+// scripts.js
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "flex";
+  setTimeout(showSlides, 10000); // Change slide every 5 seconds
+}
+
+function changeSlide(n) {
+  slideIndex += n - 1;
+  showSlides();
+}
